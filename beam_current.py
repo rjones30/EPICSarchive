c404eabf9b6e3c7b9a7fe9216cd6b8f35503d0bc
Richard Jones <rjones30@gmail.com>
2024-11-15 06:32:37 -0500

    * update beam_current.py to save results to a root tree [rtj]

--- beam_current.py
+++ beam_current.py
@@ -4,14 +4,14 @@
 #                    from the EPICS archive and plot it.
 #
 # author: richard.t.jones at uconn.edu
-# version: december 17, 2018
+# version: november 15, 2024
 
 beam_cur = {}
 
 import sys
 import math
-#import numpy
-from ROOT import *
+import numpy as np
+import ROOT
 
 import mya
 
@@ -30,14 +30,26 @@ def run_time(run):
    duration = (runobj.end_time - runobj.start_time).seconds
    return runobj.start_time.strftime("%Y-%m-%d %H:%M:%S"), duration
 
-def tseries(datetime, duration, epics_var="IBCAD00CRCUR6", deployment="ops"):
+def tfill(datetime, duration, tree=None, epics_var="IBCAD00CRCUR6", db="ops"):
    """
    Plot the rate vs time from the EPICS record for variable epics_var,
    starting at datetime, format "yyyy-mm-dd HH:MM:SS", and continuing
    from there for duration seconds. The result is returned in a TTree.
+   Argument db can be either "ops" for the recent past, or "history"
+   for dates going back before 2019.
    """
+   tepoch_s = np.zeros(1, dtype=np.uint32)
+   ibeam_nA = np.zeros(1, dtype=np.uint32)
+   if tree:
+      tree.SetBranchAddress("tepoch", tepoch_s)
+      tree.SetBranchAddress("ibeam", ibeam_nA)
+   else:
+      tree = ROOT.TTree("beamcur", "GlueX electron beam current from EPICS")
+      tree.Branch("tepoch", tepoch_s, "tepoch_s/i")
+      tree.Branch("ibeam", ibeam_nA, "ibeam_nA/i")
+
    epicstime = mya.time_string_to_epics(datetime)
-   beam_cur = mya.lookup(epics_var, deployment=deployment)
+   beam_cur = mya.lookup(epics_var, deployment=db)
    curs, times = mya.fetch(beam_cur, epicstime, duration)
    print("fetched", len(curs), "current values,", len(times), "times.")
    if len(times) > 0:
@@ -45,13 +57,13 @@ def tseries(datetime, duration, epics_var="IBCAD00CRCUR6", deployment="ops"):
    if len(times) > 1:
       print("  ...")
       print("  ", mya.time_epics_to_string(times[-1]), curs[-1])
-   return None
+   for i in range(len(times)):
+      tepoch_s[0] = times[i] // mya.epics_second
+      ibeam_nA[0] = curs[i]
+      tree.Fill()
+   return tree
 
 def help():
-   print("Example plot commands:")
-   print("  start = '2018-09-01 00:00:00'")
-   print("  duration = 10e6")
-   print("  c1 = TCanvas('c1')")
-   print("  tree = beam_currents.tseries(start,duration)")
-   print("  tree.Draw('Ibeam_nA:time_s')")
-   print("  c1.Update()")
+   print("Example usage:")
+   print("  tree = beam_current.tfill('2023-02-25 00:00:00', 10000)")
+   print("  tree.Draw('ibeam_nA:tepoch_s')")
